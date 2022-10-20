3b5e969ecb9aff2ae4531103e2ce9ec238e8ffc9
laureanomarenco <laureanomarenco@gmail.com>
2022-10-20 17:26:33 -0300

    feat/mail

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./routes/index.js');
+const cors = require('cors')
+const dotenv = require('dotenv')
+const server = express();
+
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+
+dotenv.config();
+
+var corsOptions = {
+    origin: [
+      "https://laureanomarenco.vercel.app",
+      "http://localhost:3001",
+    ],
+    headers: "*",
+    methods: "*",
+};
+
+
+server.use(cors(corsOptions));
+
+server.use('/api', routes);
+
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const { Router } = require('express')
+const nodemailer = require("nodemailer");
+const { GMAIL_PASS, GMAIL_USER } = process.env;
+
+const router = Router()
+
+router.post('/mail', async(req, res, next) => {
+    try {
+        const { email, text } = req.body;
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: GMAIL_USER,
+              pass: GMAIL_PASS,
+            },
+          });
+      
+          const mailOptions = {
+            from: "laureanomarenco@gmail.com",
+            to: "laureanomarenco@gmail.com",
+            subject: "Desde el portfolio",
+            html: `El mail: ${email} mando el siguiente texto: <br/> ${text}`,
+          };
+      
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) console.log(error);
+            else console.log("Email enviado: " + info.response);
+          });
+    } catch (error) {
+        next(err)
+    }
+})
+
+module.exports = router;
